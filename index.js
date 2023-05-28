158e74d8add66e7f3d91a9523b87bfe1e3c0f241
NIDAMUKESH <nidamukesh@gmail.com>
2023-05-28 14:45:44 +0500

    Class Task

---
+++ index.js
@@ -0,0 +1,8 @@
+//  function makeInvisible()
+//  {
+//    document.getElementById("head1").className += " hidden"
+// }
+function change()
+{
+    document.getElementById("head1").style.backgroundColor ="darksalmon";
+}
